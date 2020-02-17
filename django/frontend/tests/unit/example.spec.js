a867ad02846c795e86c3e63cd284319ce7f68800
Rick <shariq1989@gmail.com>
2020-02-17 06:18:32 -0700

    cleared boilerplate, route to login

--- django/frontend/tests/unit/example.spec.js
+++ django/frontend/tests/unit/example.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomePage from '@/components/HomePage.vue'
 
-describe('HelloWorld.vue', () => {
+describe('HomePage.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const msg = 'new message';
+    const wrapper = shallowMount(HomePage, {
       propsData: { msg }
-    })
+    });
     expect(wrapper.text()).toMatch(msg)
   })
-})
+});
