c11bf3275e84571b79a6c509b52c3377e5b3cff5
Rick <shariq1989@gmail.com>
2020-02-17 21:08:47 -0700

    add IPs to allowed_hosts

--- django/seatime_app/seatime_app/settings.py
+++ django/seatime_app/seatime_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1c*3djyuzul-oyp%3*+8z%(n^+(#nk+cs+)d6#9u+74l%#_&ev'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.0.135']
 
 
 # Application definition
