e30438b3d42a18955405681c95ca803b3b35375b
Rick <shariq1989@gmail.com>
2020-02-29 20:33:53 -0700

    syntax errors - another path error was missed

--- django/seatime_app/seatime/tests.py
+++ django/seatime_app/seatime/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from django.seatime_app.seatime.models import MarinerProfile
+from .models import MarinerProfile
 
 
 class MarinerProfileTestCase(TestCase):
