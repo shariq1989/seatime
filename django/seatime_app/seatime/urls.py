eb2ee90fb9c50adf8587954ed85311f96c61c258
Rick <shariq1989@gmail.com>
2020-03-02 22:17:14 -0700

    make viewsets work

--- django/seatime_app/seatime/urls.py
+++ django/seatime_app/seatime/urls.py
@@ -1,19 +1,18 @@
 from django.conf.urls import url, include
-from . import views
-from django.urls import path
+from . import views, admin
 from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'mariner-profiles', views.MarinerProfileViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    # url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('mariner-profile/', views.MarinerProfileList.as_view()),
-    path('mariner-profile/<int:pk>/', views.MarinerProfileDetail.as_view())
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
--- django/seatime_app/seatime/views.py
+++ django/seatime_app/seatime/views.py
@@ -1,19 +1,23 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, viewsets
+from rest_framework import viewsets, permissions
+
 from .models import MarinerProfile
 from .serializers import MarinerProfileSerializer, UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class MarinerProfileList(generics.ListCreateAPIView):
+class MarinerProfileViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
     queryset = MarinerProfile.objects.all()
     serializer_class = MarinerProfileSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
 
-
-class MarinerProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = MarinerProfile.objects.all()
-    serializer_class = MarinerProfileSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
