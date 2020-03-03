26b48c0b4cf94f4f2629564f3a7f46a810c40d35
Rick <shariq1989@gmail.com>
2020-03-03 08:09:48 -0700

    updated urls to add views

--- django/seatime_app/seatime/urls.py
+++ django/seatime_app/seatime/urls.py
@@ -8,6 +8,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'mariner-profiles', views.MarinerProfileViewSet)
+router.register(r'mariner-documents', views.MarinerProfileViewSet)
+router.register(r'vessels', views.VesselViewSet)
+router.register(r'workday-types', views.WorkdayTypeViewSet)
+router.register(r'voyage-types', views.VoyageTypeViewSet)
+router.register(r'staff-positions', views.StaffPositionViewSet)
+router.register(r'voyages', views.VoyageViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- django/seatime_app/seatime_app/urls.py
+++ django/seatime_app/seatime_app/urls.py
@@ -1,16 +1,7 @@
 from django.conf.urls import url
 from django.urls import include
-from rest_framework.documentation import include_docs_urls
-from rest_framework.schemas import get_schema_view
-
-API_TITLE = 'Pastebin API'
-API_DESCRIPTION = 'A Web API for creating and viewing highlighted code snippets.'
-schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
     url(r'^', include('seatime.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # TODO: Figure out if schema view is really needed and fix the integration issue
-    # url(r'^schema/$', schema_view),
-    url(r'^docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION))
 ]
