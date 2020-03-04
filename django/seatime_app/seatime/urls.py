daa75653a54dd673ce3309d4861f2659f3054747
Rick <shariq1989@gmail.com>
2020-03-03 21:21:34 -0700

    typo in url pointing to mariner profile

--- django/seatime_app/seatime/urls.py
+++ django/seatime_app/seatime/urls.py
@@ -8,7 +8,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'mariner-profiles', views.MarinerProfileViewSet)
-router.register(r'mariner-documents', views.MarinerProfileViewSet)
+router.register(r'mariner-documents', views.MarinerDocumentViewSet)
 router.register(r'vessels', views.VesselViewSet)
 router.register(r'workday-types', views.WorkdayTypeViewSet)
 router.register(r'voyage-types', views.VoyageTypeViewSet)
