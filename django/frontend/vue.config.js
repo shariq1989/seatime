bd23392b8a28f1a164caa62c73daa1af16f57c9b
Rick <shariq1989@gmail.com>
2020-03-08 15:31:19 -0600

    move api info to env file

--- django/frontend/vue.config.js
+++ django/frontend/vue.config.js
@@ -1,12 +1,5 @@
 module.exports = {
     "transpileDependencies": [
         "vuetify"
-    ],
-    mode: 'development',
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:8000'
-        })
-    }
+    ]
 };
