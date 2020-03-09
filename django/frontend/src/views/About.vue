cbabfade74a90fe40828e3ac7204d4a5d7cb235a
Rick <shariq1989@gmail.com>
2020-03-08 22:51:11 -0600

    remove refs to home

--- django/frontend/src/views/About.vue
+++ django/frontend/src/views/About.vue
@@ -9,14 +9,7 @@
 </template>
 
 <script>
-    // @ is an alias to /src
-    import HomePage from '@/components/HomePage.vue'
-
     export default {
-        name: 'Home',
-        components: {
-            HomePage
-        },
         data() {
             return {
                 info: null
