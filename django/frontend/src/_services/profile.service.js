16791591ec24c42489742cf48e7dc3bd643a2dd1
Rick Jamil <rick.jamil@fedex.com>
2020-03-19 22:26:23 -0600

    fix func 2

--- django/frontend/src/_services/profile.service.js
+++ django/frontend/src/_services/profile.service.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-export function getProfile() {
-    let promise = new Promise((resolve, reject) => {
-        axios({url: process.env.VUE_APP_API_URL + '/mariner-profiles/', data: {}, method: 'POST'})
-            .then(response => {
-                console.log(response);
-                resolve(response)
-            })
-            .catch(err => {
-                console.log(reject);
-                reject(err)
-            })
-    });
-}
+export let getProfile = new Promise((resolve, reject) => {
+    axios({url: process.env.VUE_APP_API_URL + '/mariner-profiles/', data: {}, method: 'POST'})
+        .then(response => {
+            console.log(response);
+            resolve(response)
+        })
+        .catch(err => {
+            console.log(reject);
+            reject(err)
+        })
+});
