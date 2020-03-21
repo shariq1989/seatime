1c81012664a03eda3b774c290b484e78153fb765
Rick <shariq1989@gmail.com>
2020-03-20 21:57:38 -0600

    getprof doesnt need an input

--- django/frontend/src/_services/profile.service.js
+++ django/frontend/src/_services/profile.service.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-export let getProfile = new Promise((resolve, reject) => {
+export function getProfile() {
     let userToken = localStorage.getItem('user');
     let headers = {"Token: ": userToken};
     axios({url: process.env.VUE_APP_API_URL + '/mariner-profiles/', method: 'GET', header: headers})
         .then(response => {
             console.log(response);
-            resolve(response)
         })
         .catch(err => {
             console.log(reject);
-            reject(err)
         })
-});
+}
