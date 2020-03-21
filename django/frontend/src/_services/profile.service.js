042171fa62707837e6560756d46b29c9e5a3c6be
Rick <shariq1989@gmail.com>
2020-03-20 22:10:10 -0600

    trying to get auth header to work

--- django/frontend/src/_services/profile.service.js
+++ django/frontend/src/_services/profile.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function getProfile() {
     let userToken = localStorage.getItem('user');
-    let headers = {"Token: ": userToken};
+    let headers = {"Authorization: ": "Token: " + userToken};
     axios({url: process.env.VUE_APP_API_URL + '/mariner-profiles/', method: 'GET', header: headers})
         .then(response => {
             console.log(response);
