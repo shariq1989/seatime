ad4e34c0d68183ce4e1998f8e310adcd8dc42ed7
Rick <shariq1989@gmail.com>
2020-03-20 21:48:45 -0600

    incorrect ref

--- django/frontend/src/_services/profile.service.js
+++ django/frontend/src/_services/profile.service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export let getProfile = new Promise((resolve, reject) => {
     let userToken = localStorage.getItem('user');
     let headers = {"Token: ": userToken};
-    axios({url: process.env.VUE_APP_API_URL + '/mariner-profiles/', method: 'GET', header: header})
+    axios({url: process.env.VUE_APP_API_URL + '/mariner-profiles/', method: 'GET', header: headers})
         .then(response => {
             console.log(response);
             resolve(response)
