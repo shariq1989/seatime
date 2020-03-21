3a123dc67470a24b3c9be52827131e96a4a0a937
Rick <shariq1989@gmail.com>
2020-03-20 22:38:07 -0600

    last shot for tonight

--- django/frontend/src/_services/profile.service.js
+++ django/frontend/src/_services/profile.service.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export function getProfile() {
-    let token = localStorage.getItem('user');
+    let token = JSON.parse(localStorage.getItem('user'));
     axios({
         url: process.env.VUE_APP_API_URL + '/mariner-profiles/',
         method: 'GET',
-        header: {'authorization': `Token ${token}`}
+        header: {"Authorization": "Token " + token}
     })
         .then(response => {
             console.log(response);
