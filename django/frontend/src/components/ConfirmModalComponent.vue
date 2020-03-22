d19db1eed9b336615a21246edd50b626a3b333ff
Rick <shariq1989@gmail.com>
2020-03-22 13:16:12 -0600

    demo child component

--- django/frontend/src/components/ConfirmModalComponent.vue
+++ django/frontend/src/components/ConfirmModalComponent.vue
@@ -1,37 +1,26 @@
 <template>
-    <div class="text-left">
-        <v-dialog v-model="confirmDialog" width="500">
-            <v-card>
-                <v-card-title class="headline grey lighten-2" primary-title>
-                    Confirm Action
-                </v-card-title>
-                <v-card-text>
-                    Are you sure you want to proceed?
-                </v-card-text>
-                <v-divider></v-divider>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="primary" text @click="confirmDialog = false">
-                        Cancel
-                    </v-btn>
-                    <v-btn color="red darken-1" text @click="confirmDialog = false">
-                        Confirm
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </div>
+    <v-dialog v-model="show" max-width="500px">
+        <v-card>
+            <v-card-actions>
+                <v-btn color="primary" flat @click.stop="show=false">Close</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
+
 <script>
     export default {
-        props: ['confirmDialog'],
-        created: function () {
-            console.log('confirmDialog from parent:');
-            console.log(this.confirmDialog) //prints out an empty string
+        props: {
+            value: Boolean
         },
         computed: {
-            testVar: function () {
-                console.log(this.confirmDialog)
+            show: {
+                get () {
+                    return this.value
+                },
+                set (value) {
+                    this.$emit('input', value)
+                }
             }
         }
     }
--- django/frontend/src/components/HomePage.vue
+++ django/frontend/src/components/HomePage.vue
@@ -38,84 +38,86 @@
             </template>
         </v-navigation-drawer>
         <v-content>
-            <modal-comp v-model="logoutDialog"/>
-            <v-container fluid class="grey lighten-4 fill-height">
-                <v-row class="mb-6">
-                    <v-col>
-                        <v-card class="pa-2">
-                            <v-card-title>
-                                Profile
-                            </v-card-title>
-                            <div v-if="profileLoading">
+            <v-btn color="accent" large @click.stop="showScheduleForm=true">
+                <ConfirmModalComponent v-model="showScheduleForm"/>
+                <v-container fluid class="grey lighten-4 fill-height">
+                    <v-row class="mb-6">
+                        <v-col>
+                            <v-card class="pa-2">
+                                <v-card-title>
+                                    Profile
+                                </v-card-title>
+                                <div v-if="profileLoading">
+                                    <v-progress-circular
+                                            indeterminate
+                                            color="primary"
+                                    ></v-progress-circular>
+                                    <v-card-text>
+                                        User Profile Loading
+                                    </v-card-text>
+                                </div>
+                                <div v-if="!profileLoading">
+                                    <v-card-text>
+                                        <p class="text-left subtitle-2 tag-title" style="margin: 0;">
+                                            Name
+                                        </p>
+                                        <p class="text-left">{{userProfile["first_name"]}}
+                                            {{userProfile["middle_name"]}}
+                                            {{userProfile["last_name"]}}</p>
+
+                                        <p class="text-left subtitle-2 tag-title" style="margin: 0;">
+                                            Birth Date
+                                        </p>
+                                        <p class="text-left">{{userProfile["birth_date"]}}</p>
+
+                                        <p class="text-left subtitle-2 tag-title" style="margin: 0;">
+                                            Mariner Reference Number
+                                        </p>
+                                        <p class="text-left">{{userProfile["mariner_ref_num"]}}</p>
+
+                                        <p class="text-left subtitle-2 tag-title" style="margin: 0;">
+                                            Citizenship
+                                        </p>
+                                        <p class="text-left">{{userProfile["citizenship_cntry"]}}</p>
+
+                                        <p class="text-left subtitle-2 tag-title" style="margin: 0;">
+                                            Residence
+                                        </p>
+                                        <p class="text-left">{{userProfile["residence_state"]}}</p>
+                                    </v-card-text>
+                                </div>
+                            </v-card>
+                        </v-col>
+                        <v-col>
+                            <v-card class="pa-2">
+                                <v-card-title>
+                                    Documents
+                                </v-card-title>
                                 <v-progress-circular
                                         indeterminate
                                         color="primary"
                                 ></v-progress-circular>
                                 <v-card-text>
-                                    User Profile Loading
+                                    User Documents Loading
                                 </v-card-text>
-                            </div>
-                            <div v-if="!profileLoading">
+                            </v-card>
+                        </v-col>
+                        <v-col>
+                            <v-card class="pa-2">
+                                <v-card-title>
+                                    Seatime Logged
+                                </v-card-title>
+                                <v-progress-circular
+                                        indeterminate
+                                        color="primary"
+                                ></v-progress-circular>
                                 <v-card-text>
-                                    <p class="text-left subtitle-2 tag-title" style="margin: 0;">
-                                        Name
-                                    </p>
-                                    <p class="text-left">{{userProfile["first_name"]}} {{userProfile["middle_name"]}}
-                                        {{userProfile["last_name"]}}</p>
-
-                                    <p class="text-left subtitle-2 tag-title" style="margin: 0;">
-                                        Birth Date
-                                    </p>
-                                    <p class="text-left">{{userProfile["birth_date"]}}</p>
-
-                                    <p class="text-left subtitle-2 tag-title" style="margin: 0;">
-                                        Mariner Reference Number
-                                    </p>
-                                    <p class="text-left">{{userProfile["mariner_ref_num"]}}</p>
-
-                                    <p class="text-left subtitle-2 tag-title" style="margin: 0;">
-                                        Citizenship
-                                    </p>
-                                    <p class="text-left">{{userProfile["citizenship_cntry"]}}</p>
-
-                                    <p class="text-left subtitle-2 tag-title" style="margin: 0;">
-                                        Residence
-                                    </p>
-                                    <p class="text-left">{{userProfile["residence_state"]}}</p>
+                                    Voyages Loading
                                 </v-card-text>
-                            </div>
-                        </v-card>
-                    </v-col>
-                    <v-col>
-                        <v-card class="pa-2">
-                            <v-card-title>
-                                Documents
-                            </v-card-title>
-                            <v-progress-circular
-                                    indeterminate
-                                    color="primary"
-                            ></v-progress-circular>
-                            <v-card-text>
-                                User Documents Loading
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                    <v-col>
-                        <v-card class="pa-2">
-                            <v-card-title>
-                                Seatime Logged
-                            </v-card-title>
-                            <v-progress-circular
-                                    indeterminate
-                                    color="primary"
-                            ></v-progress-circular>
-                            <v-card-text>
-                                Voyages Loading
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </v-container>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-container>
         </v-content>
     </v-app>
 </template>
@@ -126,7 +128,7 @@
     import ConfirmModalComponent from "./ConfirmModalComponent"
 
     export default {
-        components: {'modal-comp': ConfirmModalComponent},
+        components: {ConfirmModalComponent},
         data() {
             return {
                 drawer: true,
@@ -148,7 +150,8 @@
                 background: false,
                 profileLoading: true,
                 userProfile: {},
-                logoutDialog: false
+                logoutDialog: false,
+                showScheduleForm: false,
             }
         },
         methods: {
