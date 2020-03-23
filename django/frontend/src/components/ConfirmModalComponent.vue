76c90a2ad865878777d73f6d8bdce560fb9ea82a
Rick Jamil <rick.jamil@fedex.com>
2020-03-22 21:05:46 -0600

    minor edits to login dialog

--- django/frontend/src/components/ConfirmModalComponent.vue
+++ django/frontend/src/components/ConfirmModalComponent.vue
@@ -8,8 +8,9 @@
                 {{dialogMessage}}
             </v-card-text>
             <v-card-actions>
-                <v-btn color="primary" flat @click.stop="displayStatus=false">Cancel</v-btn>
-                <v-btn color="primary" flat @click.stop="displayStatus=false">Confirm</v-btn>
+                <v-spacer/>
+                <v-btn color="primary" @click.stop="displayStatus=false">Cancel</v-btn>
+                <v-btn color="error" @click.stop="displayStatus=false">Confirm</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
