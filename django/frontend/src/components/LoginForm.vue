f9c0a401a3f44759219d253362fbbf491861d2c1
Rick <shariq1989@gmail.com>
2020-03-24 22:06:11 -0600

    login page changes

--- django/frontend/src/components/LoginForm.vue
+++ django/frontend/src/components/LoginForm.vue
@@ -1,15 +1,11 @@
 <template>
     <v-app id="inspire">
-        <v-content>
+        <v-content fill-height fluid style="background-image: url('../assets/stock/ariel.jpg')">
             <v-row
                     align="center"
                     justify="center"
             >
-                <v-col
-                        cols="12"
-                        sm="8"
-                        md="4"
-                >
+                <v-col>
                     <v-card class="elevation-12">
                         <v-toolbar
                                 color="primary"
