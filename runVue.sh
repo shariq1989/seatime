90d2fdd2e98eaae736ccf3f24368b19d1eaf4850
Rick Jamil <rick.jamil@fedex.com>
2020-04-04 08:08:16 -0600

    modularize scripts

---
+++ runVue.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git pull
+echo "starting Vue application"
+cd ~/Documents/Development/SeaTime/django/frontend && npm run serve
---
+++ startApp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo 'Starting Django'
+sh runBackend.sh
+echo 'Starting Vue'
+sh runVue.sh
