a873ed5fedcbe023cf8c016c7e53b303b668a67a
Rick Jamil <rick.jamil@fedex.com>
2020-04-04 08:03:03 -0600

    add git pull to script

--- runApp.sh
+++ runApp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "activating python env"
 cd ~/Documents/Development/SeaTime/bin/ && source activate
+echo "pulling latest code"
+git pull
 echo "starting django webservice"
 cd ~/Documents/Development/SeaTime/django/seatime_app && python manage.py runserver 0.0.0.0:8000
