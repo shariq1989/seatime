157e72d7642503de9e3f82e9f42038ef5ac2b0dd
Rick Jamil <rick.jamil@fedex.com>
2020-04-04 21:59:19 -0600

    frontend script needed to get into the right dir before pulling from git

--- runVue.sh
+++ runVue.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+cd ~/Documents/Development/SeaTime/django/frontend
 echo "pulling latest code"
 git pull
 echo "starting Vue application"
-cd ~/Documents/Development/SeaTime/django/frontend && npm run serve
+npm run serve
