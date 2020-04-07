b8e1a42814245d6f04b8b77d5dfe639e84126971
Rick Jamil <rick.jamil@fedex.com>
2020-04-06 21:02:01 -0600

    updated the wrong method

--- django/frontend/src/_services/user.service.js
+++ django/frontend/src/_services/user.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import router from "../router";
 
 export let funcLogin = user => new Promise((resolve, reject) => {
-    axios({url: process.env.VUE_APP_API_URL + '/rest-auth/login/', data: user, method: 'POST'})
+    axios({url: process.env.VUE_APP_API_URL + '/authenticate/', data: user, method: 'POST'})
         .then(response => {
             if (response.data.key) {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
@@ -16,9 +16,8 @@ export let funcLogin = user => new Promise((resolve, reject) => {
 });
 
 export let funcRegister = registrationFields => new Promise((resolve, reject) => {
-    axios({url: process.env.VUE_APP_API_URL + '/authenticate/', data: registrationFields, method: 'POST'})
+    axios({url: process.env.VUE_APP_API_URL + '/rest-auth/registration/', data: registrationFields, method: 'POST'})
         .then(response => {
-            console.log(response);
             if (response.data.key) {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
                 localStorage.setItem('user', JSON.stringify(response.data.key));
