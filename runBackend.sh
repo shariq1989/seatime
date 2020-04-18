fcb8113b8076162f98cf1c7d6c77471f50107b39
Rick Jamil <rick.jamil@fedex.com>
2020-04-18 07:13:19 -0600

    updated runBackend script to be one step shorter and added make migrations/migrate script

--- runBackend.sh
+++ runBackend.sh
@@ -4,4 +4,4 @@ cd ~/Documents/Development/SeaTime/bin/ && source activate
 echo "pulling latest code"
 git pull
 echo "starting django webservice"
-cd ~/Documents/Development/SeaTime/django/seatime_app && python manage.py runserver 0.0.0.0:8000
+python ~/Documents/Development/SeaTime/django/seatime_app/manage.py runserver 0.0.0.0:8000
