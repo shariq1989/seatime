e197f131bb15a4b359842fcd069807578aa5853d
Rick Jamil <rick.jamil@fedex.com>
2020-04-18 08:01:39 -0600

    consolidate backend script

--- djangoMigrations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "activating python env"
-cd ~/Documents/Development/SeaTime/bin/ && source activate
-echo "make migrations"
-python ~/Documents/Development/SeaTime/django/seatime_app/manage.py makemigrations
-echo "starting django webservice"
-python ~/Documents/Development/SeaTime/django/seatime_app/manage.py migrate
--- runBackend.sh
+++ runBackend.sh
@@ -3,5 +3,9 @@ echo "activating python env"
 cd ~/Documents/Development/SeaTime/bin/ && source activate
 echo "pulling latest code"
 git pull
+echo "make migrations"
+python ~/Documents/Development/SeaTime/django/seatime_app/manage.py makemigrations
+echo "migrate changes"
+python ~/Documents/Development/SeaTime/django/seatime_app/manage.py migrate
 echo "starting django webservice"
 python ~/Documents/Development/SeaTime/django/seatime_app/manage.py runserver 0.0.0.0:8000
