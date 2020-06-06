67c1a5cfa89564f80a7f9b54436e25cb5ac75154
Rick <shariq1989@gmail.com>
2020-06-06 17:19:57 -0600

    make prod instance use postgres

--- django/seatime_app/seatime_app/settings/production.py
+++ django/seatime_app/seatime_app/settings/production.py
@@ -1,4 +1,6 @@
 from seatime_app.settings.common import *
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'Optional default value')
