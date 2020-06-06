6d163b1760869ae80cbaa17c7e5b6d23e6b8a410
Rick <shariq1989@gmail.com>
2020-06-06 12:49:02 -0600

    Create staticfiles dir for Django deploy

--- django/seatime_app/seatime_app/settings/common.py
+++ django/seatime_app/seatime_app/settings/common.py
@@ -14,6 +14,16 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -68,7 +78,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'seatime_app.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databs
 
 DATABASES = {
     'default': {
@@ -122,8 +132,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
