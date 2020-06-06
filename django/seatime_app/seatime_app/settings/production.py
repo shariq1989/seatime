1dac38de9508e24547fc8054f60034b04caede5b
Rick <shariq1989@gmail.com>
2020-06-06 14:52:27 -0600

    add heroku url to allowed_hosts

--- django/seatime_app/seatime_app/settings/production.py
+++ django/seatime_app/seatime_app/settings/production.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'Optional default value')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '45.33.106.171']
+ALLOWED_HOSTS = ['localhost', '45.33.106.171', '127.0.0.1', 'warm-ocean-91075.herokuapp.com']
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080', 'http://45.33.106.171:8080'
