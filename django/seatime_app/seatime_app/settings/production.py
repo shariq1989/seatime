be99acfc812a74cff4779873a880d51abe21e35f
Rick <shariq1989@gmail.com>
2020-06-10 21:59:44 -0600

    appending to allowed hosts didnt work as expected

--- django/seatime_app/seatime_app/settings/production.py
+++ django/seatime_app/seatime_app/settings/production.py
@@ -6,9 +6,10 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'Optional default value')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 SERVER_IP = os.getenv('DJANGO_SERVER_IP', False)
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-if SERVER_IP:
-    ALLOWED_HOSTS.append(SERVER_IP)
+if not SERVER_IP:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', SERVER_IP]
 
 if not SERVER_IP:
     CORS_ORIGIN_WHITELIST = (
