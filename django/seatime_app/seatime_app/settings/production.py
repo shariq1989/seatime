d206a185cb0c08d3201760fec624f8e370c4cf07
Rick <shariq1989@gmail.com>
2020-06-10 20:43:31 -0600

    server ip config via envar

--- django/seatime_app/seatime_app/settings/production.py
+++ django/seatime_app/seatime_app/settings/production.py
@@ -1,15 +1,20 @@
 from seatime_app.settings.common import *
-# import dj_database_url
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+SERVER_IP = os.getenv('DJANGO_SERVER_IP', False)
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if SERVER_IP:
+    ALLOWED_HOSTS.append(SERVER_IP)
 
-ALLOWED_HOSTS = ['localhost', '45.33.106.171', '127.0.0.1', 'seatime-django.herokuapp.com']
-
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8080', 'http://45.33.106.171:8080', 'http://45.33.106.171'
-)
+if not SERVER_IP:
+    CORS_ORIGIN_WHITELIST = (
+        'http://localhost:8080'
+    )
+else:
+    CORS_ORIGIN_WHITELIST = (
+        'http://localhost:8080', SERVER_IP, SERVER_IP + ':8000'
+    )
