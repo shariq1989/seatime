61b9be2756090354b032f623b35f86928991cfcf
Rick <shariq1989@gmail.com>
2020-06-10 22:22:18 -0600

    reverted that plan

--- django/seatime_app/seatime_app/settings/production.py
+++ django/seatime_app/seatime_app/settings/production.py
@@ -5,17 +5,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-SERVER_IP = os.getenv('DJANGO_SERVER_IP', False)
-if not SERVER_IP:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-else:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1', SERVER_IP]
-
-if not SERVER_IP:
-    CORS_ORIGIN_WHITELIST = (
-        'http://localhost:8080'
-    )
-else:
-    CORS_ORIGIN_WHITELIST = (
-        'http://localhost:8080', 'http://' + SERVER_IP, 'http://' + SERVER_IP + ':8000'
-    )
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '96.126.97.44']
+CORS_ORIGIN_WHITELIST = (
+        'http://localhost:8080', 'http://96.126.97.44:8080'
+)
