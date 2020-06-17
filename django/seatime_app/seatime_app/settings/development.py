996da34f98ae15956bf049067da8c16649f15390
Rick Jamil <rick.jamil@fedex.com>
2020-06-17 07:50:42 -0600

    postgres support

--- django/seatime_app/seatime_app/settings/development.py
+++ django/seatime_app/seatime_app/settings/development.py
@@ -1,6 +1,29 @@
 from seatime_app.settings.common import *
 import os
 
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databs
+
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd5acq0rca0mr73',
+        'USER': 'utywwtzdjfqwdi',
+        'PASSWORD': 'da5b1e5f9574c7c3a9d061a0a8162d6a08b33d77e4b455eee988e7222bd39eb9',
+        'HOST': 'ec2-18-214-211-47.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '1c*3djyuzul-oyp%3*+8z%(n^+(#nk+cs+)d6#9u+74l%#_&ev'
 
--- django/seatime_app/seatime_app/settings/production.py
+++ django/seatime_app/seatime_app/settings/production.py
@@ -1,5 +1,25 @@
 from seatime_app.settings.common import *
 
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd5acq0rca0mr73',
+        'USER': 'utywwtzdjfqwdi',
+        'PASSWORD': 'da5b1e5f9574c7c3a9d061a0a8162d6a08b33d77e4b455eee988e7222bd39eb9',
+        'HOST': 'ec2-18-214-211-47.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'Optional default value')
 
