0cb74a1abd6366048cbe43b9d692895b3ca79497
Rick Jamil <rick.jamil@fedex.com>
2020-06-16 23:11:34 -0600

    go back to using allowed_hosts

--- django/seatime_app/seatime_app/settings/production.py
+++ django/seatime_app/seatime_app/settings/production.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['96.126.97.44']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '96.126.97.44']
 CORS_ORIGIN_WHITELIST = (
         'http://localhost:8080', 'http://96.126.97.44:8080'
 )
