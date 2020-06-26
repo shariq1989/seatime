3df5d5f3d4b32594ef3d1cb04dc2979ab0883145
Rick <shariq1989@gmail.com>
2020-06-25 20:23:16 -0600

    encase everything in card

--- django/frontend/src/components/LoginForm.vue
+++ django/frontend/src/components/LoginForm.vue
@@ -4,73 +4,76 @@
             <v-container fill-height fluid>
                 <v-row align="center">
                     <v-row>
-                        <v-col
-                                cols="12"
-                                sm="2"
-                        >
-                        </v-col>
-                        <v-col
-                                cols="12"
-                                sm="5"
-                        >
-                            <h1>SeaTime.</h1>
-                            <p>A toolkit that allows mariners to manage various aspects of work life</p>
-                            <ul>
-                                <li>Mariner Profile</li>
-                                <li>Documents</li>
-                                <li>Voyages</li>
-                                <li>...etc...</li>
-                            </ul>
-                            <v-img src="../assets/stock/tomas-williams-p-_RJY6hN3E-unsplash.jpg" aspect-ratio="1.7"
-                                   contain></v-img>
-                        </v-col>
-                        <v-col
-                                cols="12"
-                                sm="3"
-                        >
-                            <v-form @submit.prevent="handleSubmit">
-                                <div class="pa-2">
-                                    <v-alert type="error" v-if="displayErrorMessage">
-                                        <span class="text-left" v-html="errorMessage"></span>
-                                    </v-alert>
-                                </div>
-                                <v-card-title>
-                                    Sign in or Create an Account
-                                </v-card-title>
-                                <v-card-text>
-                                    <v-text-field
-                                            label="Username"
-                                            name="login"
-                                            prepend-icon="person"
-                                            type="text"
-                                            v-model="username"
-                                            :rules="[v => !!v || 'Username is required']"
-                                            required
-                                    />
+                        <v-card>
+                            <v-col
+                                    cols="12"
+                                    sm="2"
+                            >
+                            </v-col>
+                            <v-col
+                                    cols="12"
+                                    sm="5"
+                            >
+                                <h1>Welcome to SeaTime.</h1>
+                                <p>A toolkit that allows mariners to manage various aspects of work life</p>
+                                <ul>
+                                    <li>Mariner Profile</li>
+                                    <li>Documents</li>
+                                    <li>Voyages</li>
+                                    <li>...etc...</li>
+                                </ul>
+                                <v-img src="../assets/stock/tomas-williams-p-_RJY6hN3E-unsplash.jpg" aspect-ratio="1.7"
+                                       contain></v-img>
+                            </v-col>
+                            <v-col
+                                    cols="12"
+                                    sm="3"
+                            >
+                                <v-form @submit.prevent="handleSubmit">
+                                    <div class="pa-2">
+                                        <v-alert type="error" v-if="displayErrorMessage">
+                                            <span class="text-left" v-html="errorMessage"></span>
+                                        </v-alert>
+                                    </div>
+                                    <v-card-title>
+                                        Lets Set Sail
+                                    </v-card-title>
+                                    <v-card-text>
+                                        <v-text-field
+                                                label="Username"
+                                                name="login"
+                                                prepend-icon="person"
+                                                type="text"
+                                                v-model="username"
+                                                :rules="[v => !!v || 'Username is required']"
+                                                required
+                                        />
 
-                                    <v-text-field
-                                            id="password"
-                                            label="Password"
-                                            name="password"
-                                            prepend-icon="lock"
-                                            type="password"
-                                            v-model="password"
-                                            :rules="[v => !!v || 'Password is required']"
-                                            required
-                                    />
-                                </v-card-text>
-                                <v-card-actions>
-                                    <v-btn to="/signup">Create an account</v-btn>
-                                    <v-spacer/>
-                                    <v-btn type="submit">Login</v-btn>
-                                </v-card-actions>
-                            </v-form>
-                        </v-col>
-                        <v-col
-                                cols="12"
-                                sm="2"
-                        >
-                        </v-col>
+                                        <v-text-field
+                                                id="password"
+                                                label="Password"
+                                                name="password"
+                                                prepend-icon="lock"
+                                                type="password"
+                                                v-model="password"
+                                                :rules="[v => !!v || 'Password is required']"
+                                                required
+                                        />
+                                    </v-card-text>
+                                    <v-card-actions>
+                                        <v-btn to="/signup">Create an account</v-btn>
+                                        <v-spacer/>
+                                        <v-btn type="submit">Login</v-btn>
+                                    </v-card-actions>
+                                </v-form>
+
+                            </v-col>
+                            <v-col
+                                    cols="12"
+                                    sm="2"
+                            >
+                            </v-col>
+                        </v-card>
                     </v-row>
                 </v-row>
             </v-container>
