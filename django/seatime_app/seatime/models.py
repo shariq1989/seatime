1fdc12cf6399d37d52d2866d553e58feabb66121
Rick <shariq1989@gmail.com>
2020-07-01 22:51:15 -0600

    fix field names

--- django/seatime_app/seatime/models.py
+++ django/seatime_app/seatime/models.py
@@ -69,8 +69,8 @@ class Rating(models.Model):
 
 
 class StaffRatingCombinations(models.Model):
-    staffPosition = models.ManyToManyField(StaffPosition)
-    positionRating = models.ManyToManyField(Rating)
+    staff_position = models.ManyToManyField(StaffPosition)
+    position_rating = models.ManyToManyField(Rating)
 
 
 # Info for each voyage
