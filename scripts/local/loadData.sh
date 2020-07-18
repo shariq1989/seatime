053101df950e4e593a6626077ec45127f919bc0b
Rick <shariq1989@gmail.com>
2020-07-18 14:37:47 -0600

    created loadData script

---
+++ scripts/local/loadData.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "activating python env"
+cd ~/Documents/Development/SeaTime/bin/ && source activate
+echo "pulling latest code"
+git pull
+
+heroku pg:psql postgresql-contoured-81635 --app seatime-django --command "DELETE FROM seatime_staffratingcombinations;"
+heroku pg:psql postgresql-contoured-81635 --app seatime-django --command "DELETE FROM seatime_staffposition;"
+heroku pg:psql postgresql-contoured-81635 --app seatime-django --command "DELETE FROM seatime_rating;"
+
+python3 ~/Documents/Development/SeaTime/django/seatime_app/manage.py loaddata seatime_app/seatime/fixtures/initial_data.json
+python3 ~/Documents/Development/SeaTime/django/seatime_app/manage.py loaddata seatime_app/seatime/fixtures/deck_ratings_data.json
+python3 ~/Documents/Development/SeaTime/django/seatime_app/manage.py loaddata seatime_app/seatime/fixtures/engine_ratings_data.json
