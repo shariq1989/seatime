dff779a98925ae7bf75876a01983556c13358cc8
Rick <shariq1989@gmail.com>
2020-08-09 15:36:14 -0600

    fix pkg name

--- scripts/production/createEnv.sh
+++ scripts/production/createEnv.sh
@@ -9,6 +9,6 @@ echo "activating python env"
 cd /home/development/seatime/bin/ && source activate
 cd ..
 echo "install pip3"
-sudo apt install pip3
+sudo apt install python3-pip
 echo "install requirements"
 pip3 install -r requirements.txt
