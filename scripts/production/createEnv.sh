2587be79caf05c89efe34f77415b4240174b9ddd
Rick <shariq1989@gmail.com>
2020-08-09 15:30:54 -0600

    fix typo

--- scripts/production/createEnv.sh
+++ scripts/production/createEnv.sh
@@ -2,13 +2,13 @@
 # TODO
 # cd ~/home || exit
 # mkdir development
-# sudo apt get install git
+# sudo apt install git
 # git clone git@github.com:shariq1989/seatime.git
 
 echo "activating python env"
 cd /home/development/seatime/bin/ && source activate
 cd ..
 echo "install pip3"
-sudo apt get install pip3
+sudo apt install pip3
 echo "install requirements"
 pip3 install -r requirements.txt
