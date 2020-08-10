30c9ce7a2374495020cfd17d486f59b2619889ae
Rick <shariq1989@gmail.com>
2020-08-09 19:34:22 -0600

    prod script migrations and static file scripts

--- scripts/production/createEnv.sh
+++ scripts/production/createEnv.sh
@@ -10,5 +10,15 @@ cd /home/development/seatime/bin/ && source activate
 cd ..
 echo "install pip3"
 sudo apt install python3-pip
+alias python=python3
+alias pip=pip3
+
 echo "install requirements"
 pip3 install -r requirements.txt
+
+echo "make migrations"
+python3 /home/development/seatime/django/seatime_app/manage.py makemigrations
+echo "migrate changes"
+python3 /home/development/seatime/django/seatime_app/manage.py migrate
+python3 /home/development/seatime/django/seatime_app/manage.py createsuperuser
+python3 /home/development/seatime/django/seatime_app/manage.py collectstatic
