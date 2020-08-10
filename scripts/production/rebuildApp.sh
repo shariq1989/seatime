eff5648642f4cd57dff1a3fe4ee77103c7e512eb
Rick <shariq1989@gmail.com>
2020-08-09 20:55:48 -0600

    add a script for rebuilding the app

--- scripts/production/rebuildApp.sh
+++ scripts/production/rebuildApp.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-# TODO
-# cd ~/home || exit
-# mkdir development
-# sudo apt install git
-# git clone git@github.com:shariq1989/seatime.git
-
 echo "activating python env"
 cd /home/development/seatime/bin/ && source activate
-cd ..
-echo "install pip3"
-sudo apt install python3-pip
-alias python=python3
-alias pip=pip3
 
 echo "pull latest changes"
 git -C /home/development/seatime pull
@@ -24,14 +13,5 @@ python3 /home/development/seatime/django/seatime_app/manage.py makemigrations
 echo "migrate changes"
 python3 /home/development/seatime/django/seatime_app/manage.py migrate
 
-# TODO
-echo "create super user"
-python3 /home/development/seatime/django/seatime_app/manage.py createsuperuser
-
 echo "collect static files"
 python3 /home/development/seatime/django/seatime_app/manage.py collectstatic
-
-# firewall stuff
-sudo apt install ufw
-sudo ufw allow 8000
-
