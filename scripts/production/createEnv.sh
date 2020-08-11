c17f6a73a72a6b4c38d241e763abbf538c7815a9
Rick <shariq1989@gmail.com>
2020-08-10 23:22:20 -0600

    final steps for building server

--- scripts/production/createEnv.sh
+++ scripts/production/createEnv.sh
@@ -50,3 +50,6 @@ cp -a /home/development/seatime/scripts/production/nginx/seatime /etc/nginx/site
 sudo ln -s /etc/nginx/sites-available/seatime /etc/nginx/sites-enabled
 sudo systemctl restart nginx
 
+sudo ufw delete allow 8000
+sudo ufw allow 'Nginx Full'
+
