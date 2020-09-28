05d5b04b251a7ca0e2530fe58f3bc460935df204
Rick <shariq1989@gmail.com>
2020-09-27 20:11:15 -0600

    convert template into links

--- django/frontend/src/components/ChecklistComponent.vue
+++ django/frontend/src/components/ChecklistComponent.vue
@@ -13,42 +13,38 @@
               <h1 style="font-family: serif" class="primary--text">
                 Checklists
               </h1>
-<!-- TODO              <div v-if="!pageLoading"> -->
-                <v-card-text>
-                  <div class="pa-2">
-                    <v-alert type="error" v-if="displayErrorMessage">
-                      <span class="text-left" v-html="errorMessage"></span>
-                    </v-alert>
-                    <v-snackbar v-model="snackbar">
-                      {{ snackbarText }}
-                    </v-snackbar>
-                  </div>
-                  <v-row>
-                    <v-col cols="12" sm="6" offset-sm="3">
-                      <v-card>
-                        <v-list two-line>
-                          <template v-for="(item, index) in items.slice(0, 6)">
-                            <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
-                            <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
-                            <v-list-item v-else :key="item.title">
-                              <v-list-item-avatar>
-                                <img :src="item.avatar">
-                              </v-list-item-avatar>
-                              <v-list-item-content>
-                                <v-list-item-title v-html="item.title"></v-list-item-title>
-                                <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
-                              </v-list-item-content>
-                            </v-list-item>
-                          </template>
-                        </v-list>
-                      </v-card>
-                    </v-col>
-                  </v-row>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer/>
-                </v-card-actions>
-     <!--         </div> -->
+              <!-- TODO              <div v-if="!pageLoading"> -->
+              <v-card-text>
+                <div class="pa-2">
+                  <v-alert type="error" v-if="displayErrorMessage">
+                    <span class="text-left" v-html="errorMessage"></span>
+                  </v-alert>
+                  <v-snackbar v-model="snackbar">
+                    {{ snackbarText }}
+                  </v-snackbar>
+                </div>
+                <v-row>
+                  <v-col cols="12" sm="6" offset-sm="3">
+                    <v-card>
+                      <v-list two-line>
+                        <template v-for="(item, index) in items.slice(0, 6)">
+                          <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+                          <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
+                          <v-list-item v-else :key="item.title">
+                            <v-list-item-content>
+                              <v-list-item-title v-html="item.title"></v-list-item-title>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </template>
+                      </v-list>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer/>
+              </v-card-actions>
+              <!--        for page loading </div> -->
             </v-card>
           </v-col>
           <v-col cols="12" sm="2"></v-col>
@@ -82,23 +78,13 @@ export default {
       APIMethod: 'POST',
 
       items: [
-        {header: 'Today'},
+        {header: 'Select a position'},
         {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          title: 'Brunch this weekend?',
-          subtitle: "<span class='font-weight-bold'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+          title: 'Ntl DDE',
         },
         {divider: true, inset: true},
         {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle: "<span class='font-weight-bold'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
-        },
-        {divider: true, inset: true},
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='font-weight-bold'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+          title: 'QMED',
         },
       ],
     }
